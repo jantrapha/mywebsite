a305b3265cbacd2f9758687e0141c1be32776103
jantrapha <98871829+jantrapha@users.noreply.github.com>
2022-02-28 12:52:41 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: "MyFont1";
+        src: url("Sriracha-Regular.ttf");
+      }
+      @font-face{
+        font-family: "MyFont2";
+        src: url("Charmonman-Regular.ttf");
+      }
+      #font1{
+        font-family: "MyFont1";
+      }
+      #font2{
+        font-family: "MyFont2";
+      }
+    </style>
+  </head>
+  <body>
+    <h1 id="font1">นางสาว จันทราภา ผุยป้อง ม.4/12 เลขที่ 29 เลขประจำตัว 59103</h1>
+    <h1 id="font2">นางสาว จันทราภา ผุยป้อง ม.4/12 เลขที่ 29 เลขประจำตัว 59103</h1>
+  </body>
+</html>
